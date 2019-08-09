70650aaf9c7b329bcd6852c398baefca3b0548d4
Jeanine Hoffman <jeanine.hoffman1970@gmail.com>
2019-08-08 21:44:51 -0700

    landing page built and working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    
+    return (
+      <div className="App">
+        <h1>LesFic: Rate Your Reads</h1>
+        <section>
+        <h4>The best way to search for new books in a variety of sub-genres and maybe find new authors to follow </h4>
+        <p>Start a search for a new book or a new author to follow</p>
+        </section>
+        <section>
+        <button type="button" class="goSearchBtn">Go Search</button>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
