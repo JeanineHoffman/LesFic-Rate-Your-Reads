34c64dc4a59af0174c766e5ae003e8fe3a8e8b4a
Jeanine Hoffman <jeanine.hoffman1970@gmail.com>
2019-09-20 13:47:24 -0700

    changes

--- src/searchpage/searchpage.js
+++ src/searchpage/searchpage.js
@@ -1,44 +1,63 @@
 import React, { Component } from 'react';
 import SearchAuthor from '../searchauthor/searchauthor';
 import SearchGenre from '../searchgenre/searchgenre';
-import store from "../store";
+// import store from "../store";
 import ResultsPage from '../resultspage/resultspage';
 
 
 export default class SearchPage extends Component {
   state = {
-    searchAuthor: "",
-    searchGenre: ""
-}
+    searchAuthor: null,
+    searchGenre: null
+  }
 
-updateSearchPage = (author, genre) => {
-  this.setState({
-   searchAuthor: author,
-   searchGenre: genre
- })
+  updateSearchPage = (author, genre) => {
+      this.setState({
+      searchAuthor: author,
+      searchGenre: genre
+    })
+  }
  
- 
-}
+
+
+
 render() {
  
-  const filteredBookTitles = this.state.searchAuthor ? store['names'].find(obj => { return obj.name === this.state.searchAuthor; }).titles : ''
+  const {books} = this.props;
+  
+    const filteredBookTitles = [];
+    if(this.state.searchAuthor){
+      books.forEach(obj => {
+          if(obj.author === this.state.searchAuthor){
+            filteredBookTitles.push(obj.title) 
+          }
+        });
+      }
    
-  const filteredGenreTitles = this.state.searchGenre ? store['genre'].find(obj => { return obj.genre === this.state.searchGenre; }).titles : ''
+    const filteredGenreTitles = [];
+    if(this.state.searchGenre){
+      books.forEach(obj => {
+          if(obj.genre === this.state.searchGenre){
+            filteredGenreTitles.push(obj.title) 
+          }
+        });
+      }
+   
 
     return (
       <div>
         <section>
-        <SearchAuthor store={store} updateSearch={this.updateSearchPage}
+        <SearchAuthor store={books} updateSearch={this.updateSearchPage}
         />
         <ResultsPage  list={filteredBookTitles} />
 
         </section>
         <section>
-        <SearchGenre store={store} updateSearch={this.updateSearchPage}/>
+        <SearchGenre store={books} updateSearch={this.updateSearchPage}/>
         <ResultsPage  list={filteredGenreTitles} />
+
         </section>
       </div>
     );
   }
 }
-
